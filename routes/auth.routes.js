53d27414e62df99431a3e06a892c813dcf29b57f
PolinaTsv <polina.tsvetkova7@gmail.com>
2023-09-19 15:57:43 +0700

    Basic auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -35,6 +35,44 @@ router.post("/signup", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-router.get("/userProfile", (req, res) => res.render("users/user-profile"));
+router.get("/login", (req, res) => res.render("auth/login"));
+
+router.post("/login", (req, res, next) => {
+  console.log("SESSION =====> ", req.session);
+
+  // req.body destructuring
+  // and email and password validation stay the same
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "Email is not registered. Try with other email.",
+        });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        // when we introduce session, the following line gets replaced with what follows:
+        // res.render('users/user-profile', { user });
+
+        //******* SAVE THE USER IN THE SESSION ********//
+        req.session.currentUser = user;
+        res.redirect("/userProfile");
+      } else {
+        res.render("auth/login", { errorMessage: "Incorrect password." });
+      }
+    })
+    .catch((error) => next(error));
+});
+
+router.get("/userProfile", (req, res) => {
+  res.render("users/user-profile", { userInSession: req.session.currentUser });
+});
+
+router.post("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) next(err);
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
